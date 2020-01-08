1ab38697dabeec698f8a21eb63548762e673aa45
pranusha <pranusha3493@gmail.com>
2020-01-08 08:37:46 -0700

    weather-coding-exercise -- Implemented weather components using weather service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Weather, WeatherService} from './weather.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-coding-exerise';
+
+  weathers: Weather[] = [];
+
+  constructor(private weatherService: WeatherService) {}
+
+  handleLocationsEntered(event) {
+    if (event.target.value) {
+      this.weathers = [];
+      this.displayTimeAndWeather(event.target.value.split(',').map(location => location.trim()));
+    }
+  }
+  displayTimeAndWeather(arrOfLocations: string[]) {
+    this.weatherService.getWeather(arrOfLocations).subscribe(
+      value => {
+        this.weathers = value;
+        console.log('locationsData', value);
+      },
+      err => {},
+      () => console.log('All Data loaded'),
+    );
+  }
+
+  getTimeAndWeather(weather) {
+    return this.weatherService.getTimeAndWeather(weather);
+  }
 }
